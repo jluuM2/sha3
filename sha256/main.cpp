9134edd91c8a99ccc9933d32a3a38e33cdca2dd3
jose luu <jose.luu@gmail.com>
2018-01-04 14:48:35 +0100

    more test strings in vector

--- sha256/main.cpp
+++ sha256/main.cpp
@@ -6,10 +6,18 @@ using std::string;
 using std::cout;
 using std::endl;
 
+
+
 int main(int argc, char *argv[])
 {
-	std::vector<string> input = {"grape","abc","","abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
-		"abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu",
+	std::vector<std::string> input = {"",
+		"a",
+		"abc",
+		"message digest",
+		"abcdefghijklmnopqrstuvwxyz",
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+		"12345678901234567890123456789012345678901234567890123456789012345678901234567890",
+		"The quick brown fox jumps over the lazy dog"
 	};
 
 	string output;
